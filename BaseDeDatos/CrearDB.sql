a18d378049fa1913fe9070c2820c8755d3cb7288
GThibeault <gabriel.eric.thibeault@gmail.com>
2017-04-30 18:21:46 -0300

    Creacion de la DB y pequeños cambios al MR. Falta revisar que este todo bien armado igual.

--- BaseDeDatos/CrearDB.sql
+++ BaseDeDatos/CrearDB.sql
@@ -1,33 +1,182 @@
-create table Fragancia (
-idFragancia int not null,
-nombre varchar(20),
-idTipo int not null,
-Primary Key (idFragancia)
+create table Pais (
+idPais int not null,
+nombre varchar(30),
+Primary Key (idPais)
 );
 
-create table TipoFragancia (
-idTipo int not null,
-nombre varchar(20),
-Primary Key (idTipo)
+create table Maestro (
+placaInstructor int not null,
+nombre varchar(30),
+apellido varchar(30),
+graduacion int,
+idPais int not null,
+Primary Key (placaInstructor),
+Foreign Key (idPais) References Pais(idPais)
 );
 
-create table MateriaPrima (
-idMateriaPrima int not null,
-nombre varchar(20),
-precio int,
-idProveedor int not null,
-Primary Key (idMateriaPrima, idProveedor)
+create table Escuela (
+idEscuela int not null,
+idMaestro int not null,
+Primary Key (idEscuela),
+Foreign Key (idMaestro) References Maestro(placaInstructor)
 );
 
-create table Proveedor (
-idProveedor int not null,
-nombre varchar(20),
-Primary Key (idProveedor)
+create table Estudiante (
+numCertificado int not null,
+nombre varchar(30),
+apellido varchar(30),
+genero bool,
+graduacion int,
+peso int,
+foto binary(9),
+idEscuela int not null,
+Primary Key (numCertificado),
+Foreign Key (idEscuela) References Escuela(idEscuela)
 );
 
-create table CompuestoCon (
-idFragancia int not null,
-idMateriaPrima int not null,
-cantidad int,
-Primary Key (idMateriaPrima, idFragancia)
+create table Participante (
+numCertificado int not null,
+DNI int,
+fechaDeNacimiento varchar(30),
+Primary Key (numCertificado),
+Foreign Key (numCertificado) References Estudiante(numCertificado)
+);
+
+create table Coach (
+numCertificado int not null,
+Primary Key (numCertificado),
+Foreign Key (numCertificado) References Estudiante(numCertificado)
+);
+
+create table esIntegranteDe (
+numCertificado int not null,
+idInscripcion int not null,
+esTitular bool,
+Primary Key (numCertificado, idInscripcion),
+Foreign Key (numCertificado) References Estudiante(numCertificado),
+Foreign Key (idInscripcion) References Inscripcion(idInscripcion)
+);
+
+create table Inscripcion (
+idInscripcion int not null,
+idCoach int not null,
+grupalOIndividual bool,
+Primary Key (idInscripcion),
+Foreign Key (idCoach) References Coach(numCertificado)
+);
+
+create table InscripcionIndividual (
+idInscripcion int not null,
+Primary Key (idInscripcion),
+Foreign Key (idInscripcion) References Inscripcion(idInscripcion)
+);
+
+create table InscripcionGrupal (
+idInscripcion int not null,
+nombre varchar(30),
+Primary Key (idInscripcion),
+Foreign Key (idInscripcion) References Inscripcion(idInscripcion)
+);
+
+create table esEn (
+idCompetencia int not null,
+idInscripcion int not null,
+puesto int,
+Primary Key (idCompetencia, idInscripcion),
+Foreign Key (idCompetencia) References Competencia(idCompetencia),
+Foreign Key (idInscripcion) References Inscripcion(idInscripcion)
+);
+
+create table Competencia (
+idCompetencia int not null,
+idCategoria int not null,
+cantidadDeArbitros int,
+tipo varchar(30),
+Primary Key (idCompetencia),
+Foreign Key (idCategoria) References Categoria(idCategoria)
+);
+
+create table CompetenciaFormas (
+idCompetencia int not null,
+Primary Key (idCompetencia),
+Foreign Key (idCompetencia) References Competencia(idCompetencia)
+);
+
+create table CompetenciaCombate (
+idCompetencia int not null,
+Primary Key (idCompetencia),
+Foreign Key (idCompetencia) References Competencia(idCompetencia)
+);
+
+create table CompetenciaSalto (
+idCompetencia int not null,
+Primary Key (idCompetencia),
+Foreign Key (idCompetencia) References Competencia(idCompetencia)
+);
+
+create table CompetenciaRotura (
+idCompetencia int not null,
+Primary Key (idCompetencia),
+Foreign Key (idCompetencia) References Competencia(idCompetencia)
+);
+
+create table CompetenciaCombatePorEquipos (
+idCompetencia int not null,
+Primary Key (idCompetencia),
+Foreign Key (idCompetencia) References Competencia(idCompetencia)
+);
+
+create table Categoria (
+idCategoria int not null,
+genero bool,
+Primary Key (idCategoria)
+);
+
+create table CategoriaDan (
+idCategoria int not null,
+dan int,
+Primary Key (idCategoria),
+Foreign Key (idCategoria) References Categoria(idCategoria)
+);
+
+create table CategoriaEdad (
+idCategoria int not null,
+minima int,
+maxima int,
+Primary Key (idCategoria),
+Foreign Key (idCategoria) References Categoria(idCategoria)
+);
+
+create table CategoriaPeso (
+idCategoria int not null,
+minimo int,
+maximo int,
+Primary Key (idCategoria),
+Foreign Key (idCategoria) References Categoria(idCategoria)
+);
+
+create table Ring (
+numeroDeRing int not null,
+Primary Key (numeroDeRing)
+);
+
+create table Arbitro (
+numDePlaca int not null,
+nombre varchar(30),
+apellido varchar(30),
+graduacion int,
+idPais int not null,
+Primary Key (numDePlaca),
+Foreign Key (idPais) References Pais(idPais)
+);
+
+create table esArbitradaPor (
+idCategoria int not null,
+numDePlacaArbitro int not null,
+numeroDeRing int not null,
+funcionDelArbitro varchar(30),
+Primary Key (idCategoria, numDePlacaArbitro),
+Foreign Key (idCategoria) References Categoria(idCategoria),
+Foreign Key (numDePlacaArbitro) References Arbitro(numDePlacaArbitro),
+Foreign Key (numeroDeRing) References Ring(numeroDeRing)
 );
