116971294ce77970ce4d3aeaa1f8b503506db81e
GThibeault <gabriel.eric.thibeault@gmail.com>
2017-05-01 17:35:48 -0300

    Categorias

--- BaseDeDatos/Poblar.py
+++ BaseDeDatos/Poblar.py
@@ -43,9 +43,10 @@ def generarEstudiante():
     #f = [1 for i in range(9)]
     f = "foto" #placeholder que no spammea 1s cuando hago print
     carac = [ (s, d, p, f, e)
-            for s in [0, 1]
+            for s in ["M", "F"]
             for d in range(1, 7)
-            for p in [(4 + i) * 10 for i in range(l)]
+            #for p in [(4 + i) * 10 for i in range(l)]
+            for p in [50, 60] #placeholder
             for e in range(len(list(generarEscuela())))] 
     return [ (i, *rest) for i, rest in enumerate([ (n, a, *choice(carac))
                                                  for n in listaNombres
@@ -61,26 +62,58 @@ def generarParticipantes(est):
                                                          #de un solo valor
     return part, coaches
 
+#def generarCompetencia():
+#
+#    pass
+#    #[ (i, generarCat(i, t), npr.randint(1, 5), choice(["F", "C", "S", "R", "CE"])) for i, r in enumerate(
+
+def generarCategoria():
+
+    i = 0
+    FS = [ (i + j, *r) for j, r in enumerate([ (s, cE) for s in ["M", "F"] for cE in generarCatEdad()]) ]
+    i += len(FS)
+    C = [ (i + j, *r) for j, r in enumerate([ (s, cE, p) for s in ["M", "F"] 
+                                             for cE in generarCatEdad() 
+                                             for p in generarCatPeso()]) ]
+    i += len(C)
+    RcE = [ (i + j, *r) for j, r in enumerate([ (s,) for s in ["M", "F"] ]) ]
+    i += len(RcE)
+    return { "F/S": FS, "C": C, "R/cE": RcE }
+
+def generarCatDan():
+
+    return [ v for v in range(1, 7)]
+
+def generarCatEdad():
+
+    return [ v for v in [(14, 17), (18, 35), (35, 100)]]
+
+def generarCatPeso():
+
+    return [ v for v in [(10 * i, 10 * i + 9) for i in range(5, 7) ]]
+
+
 estudiantes = generarEstudiante()
 part, coaches = generarParticipantes(estudiantes)
+generarCategoria()
 
-conn = sqlite3.connect('DB.db')
-c = conn.cursor()
-
-c.executemany('insert into Pais values (?,?)', generarPaises())
-conn.commit()
-
-c.executemany('insert into Maestro values (?,?,?,?,?)', generarMaestro())
-conn.commit()
-
-c.executemany('insert into Escuela values (?,?)', generarEscuela())
-conn.commit()
-
-c.executemany('insert into Estudiante values (?,?,?,?,?,?,?,?)', estudiantes)
-conn.commit()
-
-c.executemany('insert into Participante values (?,?,?)', part)
-conn.commit()
-
-c.executemany('insert into Coach values (?)', coaches)
-conn.commit()
+#conn = sqlite3.connect('DB.db')
+#c = conn.cursor()
+#
+#c.executemany('insert into Pais values (?,?)', generarPaises())
+#conn.commit()
+#
+#c.executemany('insert into Maestro values (?,?,?,?,?)', generarMaestro())
+#conn.commit()
+#
+#c.executemany('insert into Escuela values (?,?)', generarEscuela())
+#conn.commit()
+#
+#c.executemany('insert into Estudiante values (?,?,?,?,?,?,?,?)', estudiantes)
+#conn.commit()
+#
+#c.executemany('insert into Participante values (?,?,?)', part)
+#conn.commit()
+#
+#c.executemany('insert into Coach values (?)', coaches)
+#conn.commit()
