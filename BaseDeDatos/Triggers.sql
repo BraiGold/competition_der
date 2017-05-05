82ea917937e5ee5b7c6e31a99fb3bbab07619cba
gciruelos <gonzalo.ciruelos@gmail.com>
2017-05-05 01:31:31 -0300

    trigger arreglado

--- BaseDeDatos/Triggers.sql
+++ BaseDeDatos/Triggers.sql
@@ -212,3 +212,106 @@ CREATE TRIGGER individualEsTitular AFTER INSERT ON esIntegranteDe
           from inscripcionindividual i, esintegrantede e
           where i.idinscripcion = e.idinscripcion and not e.estitular);
     END;
+
+
+----------------------------------
+-- Consultas ---------------------
+----------------------------------
+
+select not exists (
+  select  1
+  from escuela esc
+  where not (
+        select count(p.numcertificado)
+        from participante p, estudiante e
+        where p.numcertificado = e.numcertificado and
+              e.idescuela = esc.idescuela)
+      <= 5* (
+        select count(c.numcertificado)
+        from coach c, estudiante e
+        where c.numcertificado = e.numcertificado and
+              e.idescuela = esc.idescuela));
+
+
+select not exists (
+  select 1
+  from inscripciongrupal ig
+  where not (
+    (select count(p.numcertificado)
+    from participante p, esintegrantede e
+    where e.numcertificado = p.numcertificado and
+                e.idinscripcion = ig.idinscripcion and
+                e.estitular) = 5 and (
+    select count(p.numcertificado)
+    from participante p, esintegrantede e
+    where e.numcertificado = p.numcertificado and
+                e.idinscripcion = ig.idinscripcion and
+	        not e.estitular) = 3));
+
+select not exists (
+  select 1
+  from competencia c
+  where not  (
+    select count(e.idinscripcion)
+    from esen e
+    where e.idcompetencia = c.idcompetencia) >= 3);
+
+
+select (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = @idCompetencia and e.puesto = 1) = 1 and (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = @idCompetencia and e.puesto = 2) = 1 and (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = @idCompetencia and e.puesto = 3) = 1;
+
+
+select not exists (
+select 1
+from competencia cc
+where not (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = cc.idcompetencia and e.puesto = 1) = 1 and (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = cc.idcompetencia and e.puesto = 2) = 1 and (
+  select count(e.idinscripcion)
+  from esen e
+  where e.idcompetencia = cc.idcompetencia and e.puesto = 3) = 1);
+
+
+select not exists (
+  select 1
+  from competencia cc
+  where not (
+    select count(e.numdeplacaarbitro)
+    from esarbitradapor e
+    where e.idcompetencia = cc.idcompetencia and
+          e.funciondelarbitro = "PresidenteDeMesa") >= 1 and (
+    select count(e.numdeplacaarbitro)
+    from esarbitradapor e
+    where e.idcompetencia = cc.idcompetencia and
+          e.funciondelarbitro = "Central") >= 1 and (
+    select count(e.numdeplacaarbitro)
+    from esarbitradapor e
+    where e.idcompetencia = cc.idcompetencia and
+          e.funciondelarbitro = "Juez") >= 2 and (
+    select count(e.numdeplacaarbitro)
+    from esarbitradapor e
+    where e.idcompetencia = cc.idcompetencia and
+          e.funciondelarbitro = "Suplente") >= 3);
+
+select not exists (
+  select 1
+  from competencia cc
+  where not (
+    select count(e.numdeplacaarbitro)
+    from esarbitradapor e
+    where e.idcompetencia = cc.idcompetencia) >= (
+    select c.cantidaddearbitros
+    from competencia c
+    where c.idcompetencia = cc.idcompetencia));
